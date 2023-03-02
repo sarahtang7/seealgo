50a34c8158a631e507327010ab827425885e25af
sarahtang7 <sarahtang07@gmail.com>
2023-03-02 00:41:38 -0500

    Set up Makefile, pyproject.toml, and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='see-algo',
+    version='1.0',
+    description='A Python library to visualize a data structure as it changes throughout a function',
+    packages=['see-algo'],
+    install_requires=['graphviz'],
+)
