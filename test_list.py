4c2297c4e35dfe03f7d9c9ed5cf86d8a8e5cd588
sarahtang7 <sarahtang07@gmail.com>
2023-03-02 02:44:41 -0500

    Add unit test for appending to list visualization

--- test_list.py
+++ test_list.py
@@ -19,3 +19,27 @@ def test_initList():
         true_contents = true_file.read()
 
     assert viz_contents == true_contents
+
+
+def test_appendList():
+    list1 = [1, 2, 3, 4]
+    viz1 = List()
+
+    def append_func(list):
+        list.append(5)
+        return list
+
+    see_viz1 = viz1.see(append_func, list1)
+
+    with open('see2_.gv', 'r') as file:
+        viz_contents1 = file.read()
+    with open('see3_.gv', 'r') as file:
+        viz_contents2 = file.read()
+
+    with open('outputFiles/appendListOutput1.txt', 'r') as true_file:
+        true_contents1 = true_file.read()
+    with open('outputFiles/appendListOutput2.txt', 'r') as true_file:
+        true_contents2 = true_file.read()
+
+    assert viz_contents1 == true_contents1
+    assert viz_contents2 == true_contents2
