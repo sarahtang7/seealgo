cb8493d4587a9b191862994682ba68ed295c12ce
sarahtang7 <sarahtang07@gmail.com>
2023-03-02 17:30:24 -0500

    Fix Pylint suggestings in setup.py and test_list.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
+"""
+This module contains the setup script for see-algo, as well as
+specifications for the package name, version, and installation
+requirements.
+"""
+
 from setuptools import setup
 
 setup(
     name='see-algo',
     version='1.0',
-    description='A Python library to visualize a data structure as it changes throughout a function',
+    description='''A Python library to visualize a
+                data structure as it changes throughout a function''',
     packages=['see-algo'],
     install_requires=['graphviz'],
 )
--- test_list.py
+++ test_list.py
@@ -1,159 +1,189 @@
-import pytest
+"""
+This file contains unit and integration tests for
+visualizations involving only list data structures.
+"""
 
-from graphviz import Digraph
-from typing import List
 from seealgo.seeListAlgo import List
 
 
 ### UNIT TESTS
 
-def test_initList():
+def test_initlist():
+    """
+    Test the visualization of an unchanged list.
+    """
+
     init_list = [2, 4, 6, 8, 10, 12, 14]
     viz0 = List()
-    see_viz = viz0.see(None, init_list)
+    viz0.see(None, init_list)
 
-    with open('see1_.gv', 'r') as file:
+    with open('see1_.gv', 'r', encoding='utf-8') as file:
         viz_contents = file.read()
 
-    with open('outputFiles/initListOutput.txt', 'r') as true_file:
+    with open('outputFiles/initListOutput.txt', 'r', encoding='utf-8') as true_file:
         true_contents = true_file.read()
 
     assert viz_contents == true_contents
 
 
-def test_appendList():
+def test_appendlist():
+    """
+    Test the visualization of appending
+    a value to a list.
+    """
+
     list1 = [1, 2, 3, 4]
     viz1 = List()
 
-    def append_func(list):
-        list.append(5)
-        return list
+    def append_func(lst):
+        lst.append(5)
+        return lst
 
-    see_viz1 = viz1.see(append_func, list1)
+    viz1.see(append_func, list1)
 
-    with open('see2_.gv', 'r') as file:
+    with open('see2_.gv', 'r', encoding='utf-8') as file:
         viz_contents1 = file.read()
-    with open('see3_.gv', 'r') as file:
+    with open('see3_.gv', 'r', encoding='utf-8') as file:
         viz_contents2 = file.read()
 
-    with open('outputFiles/appendListOutput1.txt', 'r') as true_file:
+    with open('outputFiles/appendListOutput1.txt', 'r', encoding='utf-8') as true_file:
         true_contents1 = true_file.read()
-    with open('outputFiles/appendListOutput2.txt', 'r') as true_file:
+    with open('outputFiles/appendListOutput2.txt', 'r', encoding='utf-8') as true_file:
         true_contents2 = true_file.read()
 
     assert viz_contents1 == true_contents1
     assert viz_contents2 == true_contents2
 
 
-def test_insertToList():
+def test_inserttolist():
+    """
+    Test the visualization of inserting
+    a value at an index within the list.
+    """
+
     list2 = [100, 200, 400, 500, 600]
     viz2 = List()
 
-    def insert_func(list):
-        list.insert(2, 300)
-        return list
+    def insert_func(lst):
+        lst.insert(2, 300)
+        return lst
 
-    see_viz2 = viz2.see(insert_func, list2)
+    viz2.see(insert_func, list2)
 
-    with open('see4_.gv', 'r') as file:
+    with open('see4_.gv', 'r', encoding='utf-8') as file:
         viz_contents1 = file.read()
-    with open('see5_.gv', 'r') as file:
+    with open('see5_.gv', 'r', encoding='utf-8') as file:
         viz_contents2 = file.read()
 
-    with open('outputFiles/insertListOutput1.txt', 'r') as true_file:
+    with open('outputFiles/insertListOutput1.txt', 'r', encoding='utf-8') as true_file:
         true_contents1 = true_file.read()
-    with open('outputFiles/insertListOutput2.txt', 'r') as true_file:
+    with open('outputFiles/insertListOutput2.txt', 'r', encoding='utf-8') as true_file:
         true_contents2 = true_file.read()
 
     assert viz_contents1 == true_contents1
     assert viz_contents2 == true_contents2
 
 
-def test_deleteFromList():
+def test_deletefromlist():
+    """
+    Test the visualization of deleting
+    a value from the list.
+    """
+
     list3 = [1, 2, 3, 3]
     viz3 = List()
 
-    def delete_func(list):
-        list.remove(3)
-        return list
+    def delete_func(lst):
+        lst.remove(3)
+        return lst
 
-    see_viz3 = viz3.see(delete_func, list3)
+    viz3.see(delete_func, list3)
 
-    with open('see6_.gv', 'r') as file:
+    with open('see6_.gv', 'r', encoding='utf-8') as file:
         viz_contents1 = file.read()
-    with open('see7_.gv', 'r') as file:
+    with open('see7_.gv', 'r', encoding='utf-8') as file:
         viz_contents2 = file.read()
 
-    with open('outputFiles/deleteListOutput1.txt', 'r') as true_file:
+    with open('outputFiles/deleteListOutput1.txt', 'r', encoding='utf-8') as true_file:
         true_contents1 = true_file.read()
-    with open('outputFiles/deleteListOutput2.txt', 'r') as true_file:
+    with open('outputFiles/deleteListOutput2.txt', 'r', encoding='utf-8') as true_file:
         true_contents2 = true_file.read()
 
     assert viz_contents1 == true_contents1
     assert viz_contents2 == true_contents2
 
 
-def test_setList():
+def test_setlist():
+    """
+    Test the visualization of assigning
+    another value to an element in the list.
+    """
+
     list4 = [2, 33, 333, 3333]
     viz4 = List()
 
-    def set_func(list):
-        list.__setitem__(0, 3)
-        return list
+    def set_func(lst):
+        lst[0] = 3
+        return lst
 
-    see_viz4 = viz4.see(set_func, list4)
+    viz4.see(set_func, list4)
 
-    with open('see8_.gv', 'r') as file:
+    with open('see8_.gv', 'r', encoding='utf-8') as file:
         viz_contents1 = file.read()
-    with open('see9_.gv', 'r') as file:
+    with open('see9_.gv', 'r', encoding='utf-8') as file:
         viz_contents2 = file.read()
 
-    with open('outputFiles/setListOutput1.txt', 'r') as true_file:
+    with open('outputFiles/setListOutput1.txt', 'r', encoding='utf-8') as true_file:
         true_contents1 = true_file.read()
-    with open('outputFiles/setListOutput2.txt', 'r') as true_file:
+    with open('outputFiles/setListOutput2.txt', 'r', encoding='utf-8') as true_file:
         true_contents2 = true_file.read()
 
     assert viz_contents1 == true_contents1
     assert viz_contents2 == true_contents2
 
-    
+
 ### INTEGRATION TEST
 
-def test_listIntegration():
-    list = [3, 53, 98, 2, 35, 46, 1]
+def test_listintegration():
+    """
+    Test the visualization of various operations
+    on the same list.
+    """
+
+    lst = [3, 53, 98, 2, 35, 46, 1]
     viz = List()
 
-    def mix_func(list):
-        list.remove(2)
-        list.append(-10)
-        list.insert(0, 0)
-        list.__setitem__(4, 1000)
-        return list
-    
-    see_viz = viz.see(mix_func, list)
+    def mix_func(lst):
+        lst.remove(2)
+        lst.append(-10)
+        lst.insert(0, 0)
+        lst[4] = 1000
+        return lst
 
-    with open('see10_.gv', 'r') as file:
+    viz.see(mix_func, lst)
+
+    with open('see10_.gv', 'r', encoding='utf-8') as file:
         viz_contents1 = file.read()
-    with open('see11_.gv', 'r') as file:
+    with open('see11_.gv', 'r', encoding='utf-8') as file:
         viz_contents2 = file.read()
-    with open('see12_.gv', 'r') as file:
+    with open('see12_.gv', 'r', encoding='utf-8') as file:
         viz_contents3 = file.read()
-    with open('see13_.gv', 'r') as file:
+    with open('see13_.gv', 'r', encoding='utf-8') as file:
         viz_contents4 = file.read()
-    with open('see14_.gv', 'r') as file:
+    with open('see14_.gv', 'r', encoding='utf-8') as file:
         viz_contents5 = file.read()
 
-    with open('outputFiles/mixListOutput1.txt', 'r') as true_file:
+    with open('outputFiles/mixListOutput1.txt', 'r', encoding='utf-8') as true_file:
         true_contents1 = true_file.read()
-    with open('outputFiles/mixListOutput2.txt', 'r') as true_file:
+    with open('outputFiles/mixListOutput2.txt', 'r', encoding='utf-8') as true_file:
         true_contents2 = true_file.read()
-    with open('outputFiles/mixListOutput3.txt', 'r') as true_file:
+    with open('outputFiles/mixListOutput3.txt', 'r', encoding='utf-8') as true_file:
         true_contents3 = true_file.read()
-    with open('outputFiles/mixListOutput4.txt', 'r') as true_file:
+    with open('outputFiles/mixListOutput4.txt', 'r', encoding='utf-8') as true_file:
         true_contents4 = true_file.read()
-    with open('outputFiles/mixListOutput5.txt', 'r') as true_file:
+    with open('outputFiles/mixListOutput5.txt', 'r', encoding='utf-8') as true_file:
         true_contents5 = true_file.read()
-    
+
     assert viz_contents1 == true_contents1
     assert viz_contents2 == true_contents2
     assert viz_contents3 == true_contents3
