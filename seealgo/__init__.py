9efd3319f47b8acaa4f779bdca61c6ba0ecb1a88
sarahtang7 <sarahtang07@gmail.com>
2023-04-14 20:03:30 -0400

    Add seealgo implementation for sets

--- seealgo/__init__.py
+++ seealgo/__init__.py
@@ -3,11 +3,13 @@ Set up relative imports.
 """
 
 from seealgo.see_tree_algo import Tree
+from seealgo.see_set_algo import Set
 from .see_list_algo import List
 
 myList = List()
 myTree = Tree()
+mySet = Set()
 
 __all__ = [
-    'List', 'Tree'
+    'List', 'Tree', 'Set'
 ]
