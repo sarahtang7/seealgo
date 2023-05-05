41f1b87d77b08fe5f60a7f310ec83d7f594b693e
sarahtang7 <sarahtang07@gmail.com>
2023-05-05 11:57:15 -0400

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='seealgo',
-    version='0.1.1',
+    version='0.2.0',
     description='''A Python library to visualize a
                 data structure as it changes throughout a function''',
     packages=['seealgo'],
