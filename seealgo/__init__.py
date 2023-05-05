c23d8a1004f64f0b8c39bbc0756ba2a9a55e8f45
sarahtang7 <sarahtang07@gmail.com>
2023-05-04 23:33:34 -0400

    Add implementations for dictionaries

--- seealgo/__init__.py
+++ seealgo/__init__.py
@@ -5,11 +5,13 @@ Set up relative imports.
 from seealgo.see_tree_algo import Tree
 from seealgo.see_set_algo import Set
 from .see_list_algo import List
+from seealgo.see_dict_algo import Dict
 
 myList = List()
 myTree = Tree()
 mySet = Set()
+myDict = Dict()
 
 __all__ = [
-    'List', 'Tree', 'Set'
+    'List', 'Tree', 'Set', 'Dict'
 ]
