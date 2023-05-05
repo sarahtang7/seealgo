fcc305a5527fff38ab625274f57fc439e9b40267
sarahtang7 <sarahtang07@gmail.com>
2023-05-04 23:44:56 -0400

    Fix linting error

--- seealgo/__init__.py
+++ seealgo/__init__.py
@@ -4,8 +4,8 @@ Set up relative imports.
 
 from seealgo.see_tree_algo import Tree
 from seealgo.see_set_algo import Set
-from .see_list_algo import List
 from seealgo.see_dict_algo import Dict
+from .see_list_algo import List
 
 myList = List()
 myTree = Tree()
